5166f05a05b7bd3d6766e61bc9aa4d58d3a07096
ambujmehra <ambujmehra16@gmail.com>
2020-08-24 20:51:34 +0530

    add room repository integration test

--- hotel-management-system-core/src/test/java/com/marriott/hms/repository/RoomRepositoryIntegrationTest.java
+++ hotel-management-system-core/src/test/java/com/marriott/hms/repository/RoomRepositoryIntegrationTest.java
@@ -1,11 +1,18 @@
 package com.marriott.hms.repository;
 
+import com.marriott.hms.dto.RoomDto;
+import com.marriott.hms.enums.RoomStatus;
+import com.marriott.hms.enums.RoomType;
+import com.marriott.hms.model.Hotel;
+import com.marriott.hms.model.Room;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -14,10 +21,36 @@ public class RoomRepositoryIntegrationTest {
     @Autowired
     private RoomRepository roomRepository;
 
+    @Autowired
+    private HotelRepository hotelRepository;
+
     @Test
     public void testAllInjectsNotNull() {
         Assert.assertNotNull(roomRepository);
     }
 
+    @Test
+    public void testfindByHotelAndRoomType() {
+        Hotel expectedHotel = hotelRepository.findByHotelName("Marriott rosymary");
+        Assert.assertEquals(expectedHotel.getHotelName(), "Marriott rosymary");
+        List<Room> expectedRoomDtos = roomRepository.findByHotelAndRoomType(expectedHotel, RoomType.SINGLE_OCCUPANCY_ROOM);
+        Assert.assertEquals(expectedRoomDtos.size(), 1);
+    }
+
+    @Test
+    public void testfindByHotelAndRoomStatus() {
+        Hotel expectedHotel = hotelRepository.findByHotelName("Marriott rosymary");
+        Assert.assertEquals(expectedHotel.getHotelName(), "Marriott rosymary");
+        List<Room> expectedRoomDtos = roomRepository.findByHotelAndRoomStatus(expectedHotel, RoomStatus.EMPTY);
+        Assert.assertEquals(expectedRoomDtos.size(), 2);
+    }
+
+    @Test
+    public void testfindByHotelAndRoomStatusAndRoomType() {
+        Hotel expectedHotel = hotelRepository.findByHotelName("Marriott rosymary");
+        Assert.assertEquals(expectedHotel.getHotelName(), "Marriott rosymary");
+        List<Room> expectedRoomDtos = roomRepository.findByHotelAndRoomStatusAndRoomType(expectedHotel, RoomStatus.EMPTY, RoomType.SINGLE_OCCUPANCY_ROOM);
+        Assert.assertEquals(expectedRoomDtos.size(), 1);
+    }
 
 }
